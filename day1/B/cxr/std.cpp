57b3d166939ff236c92e466912b03fccd944584a
2021013403 <dis@LAPTOP-7J5A3TUH.localdomain>
2024-12-14 22:48:34 +0800

    upd B data

--- day1/B/cxr/std.cpp
+++ day1/B/cxr/std.cpp
@@ -15,10 +15,11 @@ inline int read(){
     }
     return x*w;
 }
-int n,ans;
+int n,m;
 int a[N][N],b[N][N];
 int c[N][N],s[N][N],p[N][N];
 int L[N],R[N];
+char str[N];
 stack<int>sta;
 inline void debug(){
     for (int i=1;i<=n;++i){
@@ -31,19 +32,20 @@ inline int calc(int lx,int ly,int rx,int ry){
     if (lx>rx || ly>ry) return 0;
     return s[rx][ry]+s[lx-1][ly-1]-s[lx-1][ry]-s[rx][ly-1];
 }
-inline void solve(int d){
+inline int solve(int d){
     int lx=n+1,rx=0,ly=n+1,ry=0,sum=0;
+    int ans=-1;
     for (int i=1;i<=n;++i)
-        for (int j=1;j<=n;++j){
+        for (int j=1;j<=m;++j){
             p[i][j]=0;
             if (a[i][j]==b[i][j]) p[i][j]|=2;
-            if (j+d<=n && a[i][j+d]==b[i][j]) p[i][j]|=1,c[i][j]=c[i-1][j]+1;
+            if (j+d<=m && a[i][j+d]==b[i][j]) p[i][j]|=1,c[i][j]=c[i-1][j]+1;
             else c[i][j]=0;
             if (!p[i][j]){
                 lx=min(lx,i),rx=max(rx,i);
-                if (j-d<=0) return;
+                if (j-d<=0) return -1;
                 ly=min(ly,j-d),ry=max(ry,j-d);
-                if (ry-ly+1>d) return;
+                if (ry-ly+1>d) return -1;
             }
             s[i][j]=s[i-1][j]+s[i][j-1]-s[i-1][j-1]+(p[i][j]==1);
             sum+=(p[i][j]==1);
@@ -52,40 +54,44 @@ inline void solve(int d){
     for (int i=1;i<=n;++i){
         if (i<rx) continue;
         sta=stack<int>();
-        for (int j=1;j<=n;++j){
+        for (int j=1;j<=m;++j){
             while (!sta.empty() && c[i][sta.top()]>=c[i][j]) sta.pop();
             if (sta.empty()) L[j]=1;
             else L[j]=sta.top()+1;
             sta.push(j);
         }
         sta=stack<int>();
-        for (int j=n;j>=1;--j){
+        for (int j=m;j>=1;--j){
             while (!sta.empty() && c[i][sta.top()]>=c[i][j]) sta.pop();
-            if (sta.empty()) R[j]=n;
+            if (sta.empty()) R[j]=m;
             else R[j]=sta.top()-1;
             sta.push(j);
         }
-        for (int j=1;j<=n;++j){
+        for (int j=1;j<=m;++j){
             //printf("%d %d %d %d\n",i,j,L[j],R[j]);
             if (i-c[i][j]+1>lx) continue;
-            int nowl=L[j],nowr=min(n-d,R[j]);
+            int nowl=L[j],nowr=min(m-d,R[j]);
             if (nowl>ly || nowr<ry) continue; 
             int resl=max(nowr+1,nowl+d),resr=nowr+d;
             if (calc(i-c[i][j]+1,resl,i,resr)+calc(i-c[i][j]+1,nowl,i,nowr)!=sum) continue;   
             ans=max(ans,(nowr-nowl+1)*c[i][j]);
         }
     }
+    return ans;
 }
 int main(){
-    n=read();
-    for (int i=1;i<=n;++i)
-        for (int j=1;j<=n;++j)
-            a[i][j]=read();
-    for (int i=1;i<=n;++i)
-        for (int j=1;j<=n;++j)
-            b[i][j]=read();
-    for (int i=1;i<=n;++i)
-        solve(i);
-    printf("%d\n",ans);
+    n=read(),m=read();
+    for (int i=1;i<=n;++i){
+        scanf("%s",str+1);
+        for (int j=1;j<=m;++j)
+            a[i][j]=str[j]-'0';
+    }
+    for (int i=1;i<=n;++i){
+        scanf("%s",str+1);
+        for (int j=1;j<=m;++j)
+            b[i][j]=str[j]-'0';
+    }
+    for (int i=1;i<m;++i)
+        printf("%d%c",solve(i),i==m-1?'\n':' ');
     return 0;
 }
